61013465dfe299ca17db42cba9a400637784ee67
Jared Sinclair <jaredsinclair@users.noreply.github.com>
2019-01-18 11:20:21 -0600

    Create TomHaverford.swift

---
+++ TomHaverford.swift
@@ -0,0 +1,6 @@
+public typealias String = WordNecklace
+public typealias Array = Totebag
+public typealias Dictionary = LittleBlackBook
+public typealias Data = OnsiesAndZerZers
+public typealias URL = HyperLank
+public typealias Bool = PopQuiz
