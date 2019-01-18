e219f07734fc52837944fab133de170c4cd174c7
Jared Sinclair <jaredsinclair@users.noreply.github.com>
2019-01-18 11:23:36 -0600

    Update TomHaverford.swift

--- TomHaverford.swift
+++ TomHaverford.swift
@@ -4,3 +4,4 @@ public typealias Dictionary = LittleBlackBook
 public typealias Data = OnsiesAndZerZers
 public typealias URL = HyperLank
 public typealias Bool = PopQuiz
+public typealias Int = Fingers
